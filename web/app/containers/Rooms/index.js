aa43187c6946308198bdac8d6571ac889905441c
Jeison Borja <Jeison.Borja@endava.com>
2018-04-06 12:21:20 -0500

    Added rooms

---
+++ web/app/containers/Rooms/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Rooms extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>Test Rooms</div>
+    );
+  }
+}
